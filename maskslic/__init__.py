7acbeecc0763ddcec5fad6780396aa8d4b50ec56
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-07-09 11:44:17 +0100

    Expose version and timestamp

--- maskslic/__init__.py
+++ maskslic/__init__.py
@@ -1,3 +1,10 @@
-from .slic_superpixels import slic
+"""
+maskSLIC: Simple linear iterative clustering (SLIC) in a region of interest (ROI)
 
-__all__ = ['slic']
+Copyright (C) 2016-2019, Benjamin Irving
+"""
+
+from .slic_superpixels import slic
+from ._version import __version__, __timestamp__
+
+__all__ = ['slic', '__version__', '__timestamp__']

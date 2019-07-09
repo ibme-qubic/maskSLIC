b753d8132e8e3dce1b66daba51d47424e0e04dfe
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-07-09 11:16:36 +0100

    Move example out of root dir

--- examples/run_example.py
+++ examples/run_example.py
@@ -1,15 +1,22 @@
+"""
+maskSLIC: Example usage
 
+Copyright (C) 2016-2019 Benjamin Irving
 
-
+See LICENSE.txt for more details
+"""
 from __future__ import division, absolute_import, unicode_literals, print_function
+
+import time
 
 import matplotlib.pyplot as plt
 
-import maskslic as seg
 import numpy as np
+
 from skimage.segmentation import mark_boundaries
 from skimage.io import imread
-import time
+
+import maskslic as seg
 
 # Load image
 img = imread('chelsea.png')
@@ -18,7 +25,9 @@ img = imread('chelsea.png')
 # But the roi input input maskSLIC should be a binary image with the same spatial
 # Dimensions as the image (in this case 300x451)
 roi = imread('chelsea_mask.png')
-# The alpha channel is used to store the ROI in this case and is converted into a logical array of 0s and 1s
+
+# The alpha channel is used to store the ROI in this case and is converted into a 
+# logical array of 0s and 1s
 roi = roi[:, :, 3] > 0
 
 # Alternatively a mask could be created manually with for example a disk:
@@ -29,13 +38,13 @@ roi = roi[:, :, 3] > 0
 # mask = x*x + y*y <= r*r
 # roi[mask] = 1
 
-
 # ~~~~~~~~~~~~ Example 1: maskSLIC ~~~~~~~~~~~~~
 
 t1 = time.time()
-# Note that compactness is defined differently because a grid is not used. Lower compactness for maskSLIC is equivalent
+# Note that compactness is defined differently because a grid is not used. Lower compactness 
+# for maskSLIC is equivalent
 segments = seg.slic(img, compactness=10, seed_type='nplace', mask=roi, n_segments=12,
-                    recompute_seeds=True, plot_examples=True, enforce_connectivity=True)
+                    recompute_seeds=True, enforce_connectivity=True)
 print("Time: {:.2f} s".format(time.time() - t1))
 
 plt.figure()
@@ -47,8 +56,8 @@ plt.axis('off')
 # ~~~~~~~~~~~ Example 2: SLIC ~~~~~~~~~~~~~~~~~
 
 t1 = time.time()
-segments = seg.slic(img, compactness=30, seed_type='grid', n_segments=80, plot_examples=False, enforce_connectivity=True)
-# segments[roi==0] = -1
+segments = seg.slic(img, compactness=30, seed_type='grid', n_segments=80, 
+                    enforce_connectivity=True)
 print("Time: {:.2f} s".format(time.time() - t1))
 
 plt.figure()

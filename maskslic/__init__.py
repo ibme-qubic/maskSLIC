69dd901a3753e5d69b7fdfb3b20fd38e8ba9da4d
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-07-09 13:57:24 +0100

    Added perfslic components to do supervoxels on 4D perfusion data

--- maskslic/__init__.py
+++ maskslic/__init__.py
@@ -3,8 +3,8 @@ maskSLIC: Simple linear iterative clustering (SLIC) in a region of interest (ROI
 
 Copyright (C) 2016-2019, Benjamin Irving
 """
-
 from .slic_superpixels import slic
+from .perfslic import perfslic
 from ._version import __version__, __timestamp__
 
-__all__ = ['slic', '__version__', '__timestamp__']
+__all__ = ['slic', 'perfslic', '__version__', '__timestamp__']
